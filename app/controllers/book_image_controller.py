d8bb48fc73aed2828e17c660af461fdc43fec57b
YuryXXl <y7183605555@icloud.com>
2025-06-30 16:15:48 -0500

    Done all

--- app/controllers/book_image_controller.py
+++ app/controllers/book_image_controller.py
@@ -1,10 +1,14 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required
-from app.services.book_image_service import upload_image
+from app.services.book_image_service import BookImageService
 
 book_image_bp = Blueprint("book_image_bp", __name__)
+book_image_service = BookImageService()
 
 @book_image_bp.route("/<int:book_id>/images", methods=["POST"])
-@jwt_required()
 def upload_book_image(book_id):
-    return upload_image(book_id, request)
+    file = request.files["file"]
+    is_primary = request.form.get("is_primary", "false").lower() == "true"
+    caption = request.form.get("caption")
+
+    return jsonify(book_image_service.upload_book_image(book_id, file, is_primary, caption).to_dict()), 201
--- app/utils/cloudinary_utils.py
+++ app/utils/cloudinary_utils.py
@@ -4,16 +4,14 @@ from pathlib import Path
 import cloudinary
 import cloudinary.uploader
 
-# Загрузка .env
 env_path = Path('.') / '.env'
 # load_dotenv(dotenv_path=env_path)
 
-# Проверка, что переменные загружаются
+
 print("DEBUG: cloud_name =", os.getenv("CLOUDINARY_CLOUD_NAME"))
 print("DEBUG: api_key =", os.getenv("CLOUDINARY_API_KEY"))
 print("DEBUG: api_secret =", os.getenv("CLOUDINARY_API_SECRET"))
 
-# Настройка cloudinary
 cloudinary.config(
     cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
     api_key=os.getenv("CLOUDINARY_API_KEY"),
@@ -22,4 +20,7 @@ cloudinary.config(
 
 def upload_image_to_cloudinary(file):
     result = cloudinary.uploader.upload(file)
-    return result["secure_url"]
+    return {
+        "secure_url": result["secure_url"],
+        "public_id": result["public_id"]
+    }
